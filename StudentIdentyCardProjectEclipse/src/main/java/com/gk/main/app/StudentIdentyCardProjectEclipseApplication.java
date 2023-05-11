b5713d2556c8fce4fbc45d780bdb5a2313211e74
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-11 14:47:54 +0530

    Project Initialisation

---
+++ StudentIdentyCardProjectEclipse/src/main/java/com/gk/main/app/StudentIdentyCardProjectEclipseApplication.java
@@ -0,0 +1,15 @@
+package com.gk.main.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentIdentyCardProjectEclipseApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("StudentIdentyCard");
+		SpringApplication.run(StudentIdentyCardProjectEclipseApplication.class, args);
+	}
+
+}
