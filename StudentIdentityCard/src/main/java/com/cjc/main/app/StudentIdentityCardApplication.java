71076085078fbbac80cee50ee90ff84b36917fcb
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-11 15:29:47 +0530

    Initialising

---
+++ StudentIdentityCard/src/main/java/com/cjc/main/app/StudentIdentityCardApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.main.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentIdentityCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentIdentityCardApplication.class, args);
+	}
+
+}
---
+++ StudentIdentityCard/src/test/java/com/cjc/main/app/StudentIdentityCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.main.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentIdentityCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
